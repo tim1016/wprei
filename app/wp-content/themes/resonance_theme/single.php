8087e9c8b72dfe3f59fd4e2ab9e09b31a24b62a1
tim1016 <inkant@gmail.com>
2018-04-18 11:24:29 -0500

    Styling single.php

--- app/wp-content/themes/resonance_theme/single.php
+++ app/wp-content/themes/resonance_theme/single.php
@@ -5,13 +5,30 @@
 while(have_posts()){
     the_post(); 
     ?>
-    <h2> <?php the_title();?> </h2>
-    <p><?php the_content();?></p>
-    <hr>
+    <section class="banner">
+        <div class="banner--interior ">                    
+            <div class="row">
+                <div class="banner__box">
+                    <h1 class="display-1 display-1--main moveinleft"><?php the_title();?></h1>
+                    <h1 class="display-1 display-1--sub moveinright">subtitle</h1>               
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section">
+        
+        <div class="row generic-text">
+            <div class="metabox">
+                <p>Posted by <?php echo the_author_posts_link( );?> on <?php the_time('n.j.y')?> in <?php echo get_the_category_list( ', ' )?></p>
+            </div>
+            <?php the_content( );?>
+        </div>
+    </section>
+
     <?php
-
 }
-
 ?>
 
 <?php get_footer();?>
