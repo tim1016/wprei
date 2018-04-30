69fbd33daaca038c7ced296f88413a0eabe4ce34
tim1016 <inkant@gmail.com>
2018-04-30 12:00:45 -0500

    implemented get_template_parts

--- app/wp-content/themes/resonance_theme/front-page.php
+++ app/wp-content/themes/resonance_theme/front-page.php
@@ -138,10 +138,11 @@
                 <div class="col-1-of-2">
 
                     <h2 class="heading-3 u-center-text u-margin-bottom-medium">Upcoming Events</h2>
+
                     <?php 
                         $today = date('Ymd');
                         $homepageEvents = new WP_Query(array(
-                            'posts_per_page' => -1,
+                            'posts_per_page' => 2,
                             'post_type' => 'event',
                             'meta_key' => 'event_date',
                             'orderby' => 'meta_value_num',
@@ -156,37 +157,21 @@
                             )
                         ));
 
-                        while($homepageEvents->have_posts() ) {
-                            $homepageEvents->the_post(); ?>
-                            <div class="event-summary">
-                                <a class="event-summary__date u-center-text" href="<?php the_permalink();?>">
-                                    <span class="event-summary__month">
-                                    <?php 
-                                        $eventDate = new DateTime(get_field('event_date'));
-                                        echo $eventDate->format('M');
-                                    ?>
-                                    </span>
-                                    <span class="event-summary__day"><?php 
-                                        $eventDate = new DateTime(get_field('event_date'));
-                                        echo $eventDate->format('d');
-                                    ?>
-                                    </span>  
-                                </a>
-                                <div class="event-summary__content">
-                                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-                                    <p>
-                                        <?php
-                                            if(has_excerpt()){
-                                                echo get_the_excerpt(); //the_excerpt may add extra <p> </p> tag in the markup
-                                            }else{
-                                                echo wp_trim_words( get_the_content(),20);
-                                            }
-                                        ?> 
-                                        <a href="<?php the_permalink();?>" class="nu gray">Learn more</a>
-                                    </p>
-                                </div>
-                            </div>
-                        <?php
+                        while($homepageEvents->have_posts()) {
+                            $homepageEvents->the_post();
+                            get_template_part('template-parts/content', 'event');
+                            ?>
+<div class="event-summary">
+    <a class="event-summary__date event-summary__date--beige u-center-text" href="<?php the_permalink();?>">
+        <span class="event-summary__month"> <?php the_time('M')?>  </span>
+        <span class="event-summary__day"><?php the_time('j')?> </span>  
+    </a>
+    <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
+        <p><?php echo wp_trim_words( get_the_content(), 18)?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
+    </div>
+</div> 
+                            <?php
                         }
                     ?>
 
