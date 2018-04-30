881d80b216d8b90bf5cf91c92871f4809358a1cb
tim1016 <inkant@gmail.com>
2018-04-30 12:50:04 -0500

    Completed get_template_parts

--- app/wp-content/themes/resonance_theme/front-page.php
+++ app/wp-content/themes/resonance_theme/front-page.php
@@ -160,18 +160,6 @@
                         while($homepageEvents->have_posts()) {
                             $homepageEvents->the_post();
                             get_template_part('template-parts/content', 'event');
-                            ?>
-<div class="event-summary">
-    <a class="event-summary__date event-summary__date--beige u-center-text" href="<?php the_permalink();?>">
-        <span class="event-summary__month"> <?php the_time('M')?>  </span>
-        <span class="event-summary__day"><?php the_time('j')?> </span>  
-    </a>
-    <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-        <p><?php echo wp_trim_words( get_the_content(), 18)?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
-    </div>
-</div> 
-                            <?php
                         }
                     ?>
 
--- app/wp-content/themes/resonance_theme/template-parts/content-event.php
+++ app/wp-content/themes/resonance_theme/template-parts/content-event.php
@@ -1,10 +1,28 @@
 <div class="event-summary">
-    <a class="event-summary__date event-summary__date--beige u-center-text" href="<?php the_permalink();?>">
-        <span class="event-summary__month"> <?php the_time('M')?>  </span>
-        <span class="event-summary__day"><?php the_time('j')?> </span>  
+    <a class="event-summary__date u-center-text" href="<?php the_permalink();?>">
+        <span class="event-summary__month">
+        <?php 
+            $eventDate = new DateTime(get_field('event_date'));
+            echo $eventDate->format('M');
+        ?>
+        </span>
+        <span class="event-summary__day"><?php 
+            $eventDate = new DateTime(get_field('event_date'));
+            echo $eventDate->format('d');
+        ?>
+        </span>  
     </a>
     <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-        <p><?php echo wp_trim_words( get_the_content(), 18)?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
+        <p>
+            <?php
+                if(has_excerpt()){
+                    echo get_the_excerpt(); //the_excerpt may add extra <p> </p> tag in the markup
+                }else{
+                    echo wp_trim_words( get_the_content(),20);
+                }
+            ?> 
+            <a href="<?php the_permalink();?>" class="nu gray">Learn more</a>
+        </p>
     </div>
-</div>  
+</div>
